78d1a8c77730fb7f850098054cc381da03e9b826
lchoward <lchoward@users.noreply.github.com>
2022-01-12 21:08:16 -0500

    added font and some basic config

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import './index.css';
+// import CommandBar from '@commandbar/stable';
 
 function App() {
+  window.CommandBar.addCallback("startWorkflow", (args, context) => {
+    window.open('/foo.com/page1/' + args.company.slug);
+    window.open('/foo.com/page2/' + args.company.slug);
+    window.open('/foo.com/page3/' + args.company.slug);
+  });
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          {/* Learn React */}
         </a>
       </header>
     </div>
+    
   );
 }
 
